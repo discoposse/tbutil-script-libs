15ca41d30b5b625e95337bf43cd317c521ae72c8
Ryan Geyer <ryan.geyer@turbonomic.com>
2019-03-15 12:03:20 -0700

    Added function for parsing tbutil commit and build dates

--- utilities.js
+++ utilities.js
@@ -1,3 +1,4 @@
+/*globals exports*/
 /*jslint plusplus: true*/
 
 exports.maskObject = function (obj, mask) {
@@ -13,4 +14,17 @@ exports.maskObject = function (obj, mask) {
         }
     }
     return return_obj;
+};
+
+exports.tbutil_date_string_to_date_obj = function (date_str) {
+    "use strict";
+    var yr, mo, dy, hr, mi, se;
+    yr = parseInt(date_str.substring(0, 4), 10);
+    mo = parseInt(date_str.substring(5, 7), 10);
+    dy = parseInt(date_str.substring(8, 10), 10);
+    hr = parseInt(date_str.substring(11, 13), 10);
+    mi = parseInt(date_str.substring(14, 16), 10);
+    se = parseInt(date_str.substring(17, 19), 10);
+
+    return new Date(yr, mo, dy, hr, mi, se);
 };
